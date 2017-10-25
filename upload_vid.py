6f1ab7c39e770c94a2b5f7e827e9952e6d86891d
Sybren A. Stüvel <sybren@stuvel.eu>
2017-10-25 15:26:32 +0200

    Updated upload_vid.py to work with current youtube-upload
    This now passes CLI arguments instead of hacking them in, so it should be
    more reliable and easier to maintain. It also sets the title card as
    thumbnail.

--- upload_vid.py
+++ upload_vid.py
@@ -3,7 +3,6 @@
 # Run 'youtube_uploader' on CLI first.
 
 import argparse
-import webbrowser
 import glob
 
 import youtube_upload.main
@@ -13,46 +12,38 @@ parser = argparse.ArgumentParser()
 parser.add_argument('talk_id')
 args = parser.parse_args()
 
-# Figure out which video to upload
-the_glob = 'upload-ready-vids/%s-*.mkv' % args.talk_id
-candidate_files = glob.glob(the_glob)
-if len(candidate_files) > 1:
-    raise SystemExit('Multiple matches:\n' + '\n'.join(candidate_files))
-if not candidate_files:
-    raise SystemExit('No files match %s' % the_glob)
-video_fname = candidate_files[0]
+def find_one(the_glob: str) -> str:
+    candidate_files = glob.glob(the_glob)
+    if len(candidate_files) > 1:
+        raise SystemExit('Multiple matches:\n' + '\n'.join(candidate_files))
+    if not candidate_files:
+        raise SystemExit('No files match %s' % the_glob)
+    return candidate_files[0]
+
+# Figure out which video to upload and with what thumbnail.
+video_fname = find_one('upload-ready-vids/%s-*.mkv' % args.talk_id)
+png_fname = find_one('cards/png/%s-*.png' % args.talk_id)
 
 # Load the prezzo JSON
 presentations = gen_cards.load_json()
 pres = presentations[args.talk_id]
 
-
-# Mocking CLI options, see youtube_upload.main.main()
-class Options:
-    title = pres['title']
-    description = '''"%(title)s" by %(speakers)s
+description = '''"%(title)s" by %(speakers)s
 Blender Conference 2017
 %(day)s %(time)s at the %(location)s.
-'''
-    privacy = 'unlisted'  # TODO: change to 'public'
-    playlist = 'Blender Conference 2017'
-    publish_at = None
-    location = None
-    recording_date = None
-    default_language = None
-    default_audio_language = None
-    client_secrets = None
-    credentials_file = None
-    auth_browser = False
-    tags = None
-    title_template = None
-    category = None
+''' % pres
 
-options = Options()
-options.description %= pres
+# Mocking CLI options, see youtube_upload.main.main()
+argv = [
+    '--title', pres['title'],
+    '--description', description,
+    '--privacy', 'unlisted',
+    '--playlist', 'Blender Conference 2017',
+    '--client-secrets', 'youtube-upload/client_secrets.json',
+    '--thumbnail', png_fname,
+    '--open-link',
+    video_fname,
+]
+youtube_upload.main.main(argv)
 
-youtube = youtube_upload.main.get_youtube_handler(options)
-
-video_id = youtube_upload.main.upload_youtube_video(youtube, options, video_fname, 1, 0)
-video_url = youtube_upload.main.WATCH_VIDEO_URL.format(id=video_id)
-webbrowser.open_new_tab(video_url)
+print('DØNER')
